ba7fdeddf5e08f2c349d15a2024bc933fb282083
ismail <coderismail22@gmail.com>
2024-02-24 13:00:21 +0600

    useReducer-base-structure-done

---
+++ src/reducers/cartReducer.jsx
@@ -0,0 +1,13 @@
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "SAME-CASE-NAME":
+      return {
+        ...state, //Shallow copy of state
+        cart: [...state.cart, action.payload], // "state" is an "object" & "action.payload" is the data "previous state"
+      };
+    default:
+      return state; // Needed for Switch
+  }
+};
+
+export default cartReducer;
